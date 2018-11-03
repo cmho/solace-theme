009cbe27a4426b296a890656514c89f14c0053d8
Carly Ho <yaysunshine@gmail.com>
2018-11-03 00:39:35 -0500

    add content wrapper

--- resources/views/partials/content-merits.blade.php
+++ resources/views/partials/content-merits.blade.php
@@ -1,7 +1,9 @@
-@if(App\Merits::list())
-  @foreach(App\Merits::list() as $post)
-    <h3>{{ get_the_title() }} {{ join(", ", get_field('allowed_ratings')) }}</h3>
-    {{ get_field('prerequisites') ? '<strong>Prerequisites:</strong> '.get_field('prerequisites') : '' }}
-    {{ get_the_content() }}
-  @endforeach
-@endif
+<article @php post_class('content') @endphp>
+  @if(App\Merits::list())
+    @foreach(App\Merits::list() as $post)
+      <h3>{{ get_the_title() }} {{ join(", ", get_field('allowed_ratings')) }}</h3>
+      {{ get_field('prerequisites') ? '<strong>Prerequisites:</strong> '.get_field('prerequisites') : '' }}
+      {{ get_the_content() }}
+    @endforeach
+  @endif
+</article>
