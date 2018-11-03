3bd7c3bbcb3d5dda6d2711cd668c9764816c3809
Carly Ho <yaysunshine@gmail.com>
2018-11-03 14:37:49 -0500

    set up content for dramatis personae

---
+++ app/controllers/characters.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Sober\Controller\Controller;
+
+class Characters extends Controller
+{
+    public static function list()
+    {
+        $args = array(
+            'posts_per_page' => -1,
+            'post_type' => 'character',
+            'orderby' => 'title',
+            'order' => 'ASC',
+            'meta_query' => array(
+                array(
+                    'key' => 'is_secret',
+                    'value' => true,
+                    'compare' => '!='
+                )
+            )
+        );
+
+        return \get_posts($args);
+    }
+}
---
+++ resources/views/template-dramatis.blade.php
@@ -0,0 +1,22 @@
+{{--
+  Template Name: Dramatis Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    <section class="grey">
+      <div class="wrapper">
+        <div class="row center-xs">
+          <div class="col-xs-12">
+            @include('partials.page-header')
+          </div>
+        </div>
+        <div class="row">
+          @include('partials.content-dramatis')
+        </div>
+      </div>
+    </section>
+  @endwhile
+@endsection
