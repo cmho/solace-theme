cda9b2a02c217e1b5e5a4084bdf2a8c7d48d8893
Carly Ho <yaysunshine@gmail.com>
2018-11-03 13:55:42 -0500

    setup postdata correctly on merit pages

--- resources/views/partials/content-merits.blade.php
+++ resources/views/partials/content-merits.blade.php
@@ -1,9 +1,11 @@
 <article>
   @if(App\Merits::list())
     @foreach(App\Merits::list() as $post)
-      <h3>{{ get_the_title() }} {{ join(", ", get_field('allowed_ratings')) }}</h3>
+      @php(setup_postdata($post))
+      <h3>{{ get_the_title() }} {{ App\Merit::dots() }}</h3>
       {{ get_field('prerequisites') ? '<strong>Prerequisites:</strong> '.get_field('prerequisites') : '' }}
       {{ get_the_content() }}
     @endforeach
+    @php(wp_reset_postdata())
   @endif
 </article>
