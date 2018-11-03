0a64d979ff82c766fb215207f5f54cfc19d86f6c
Carly Ho <yaysunshine@gmail.com>
2018-11-03 13:18:37 -0500

    add single downtime content partial, add logic to redirect user if not allowed

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,3 +1,14 @@
+@php
+  $user = wp_get_current_user();
+  $is_author = $post->post_author == wp_get_current_user()->ID;
+  $is_admin = in_array('administrator', $user->roles);
+  if (in_array(get_post_type(), array('downtime', 'character'))) {
+    if (!$is_author && !$is_admin) {
+      header('Location:'.home_url('/'));
+    }
+  }
+@endphp
+
 @extends('layouts.app')
 
 @section('content')
