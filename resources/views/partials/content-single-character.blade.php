6d76ede8bf99a209d4ef01bbf7b7ee95ac945620
Carly Ho <yaysunshine@gmail.com>
2018-11-04 15:39:06 -0600

    add the rest of the skills

--- resources/views/partials/content-single-character.blade.php
+++ resources/views/partials/content-single-character.blade.php
@@ -182,6 +182,104 @@
               @php(printDots(get_field('science')))
             </div>
           </div>
+          <h4>Physical</h4>
+          <div class="row between-xs middle-xs">
+            <label>Athletics</label>
+            <div class="dots">
+              @php(printDots(get_field('athletics')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Brawl</label>
+            <div class="dots">
+              @php(printDots(get_field('brawl')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Drive</label>
+            <div class="dots">
+              @php(printDots(get_field('drive')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Firearms</label>
+            <div class="dots">
+              @php(printDots(get_field('firearms')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Larceny</label>
+            <div class="dots">
+              @php(printDots(get_field('larceny')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Stealth</label>
+            <div class="dots">
+              @php(printDots(get_field('stealth')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Survival</label>
+            <div class="dots">
+              @php(printDots(get_field('survival')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Weaponry</label>
+            <div class="dots">
+              @php(printDots(get_field('weaponry')))
+            </div>
+          </div>
+          <h4>Social</h4>
+          <div class="row between-xs middle-xs">
+            <label>Animal Ken</label>
+            <div class="dots">
+              @php(printDots(get_field('animal_ken')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Empathy</label>
+            <div class="dots">
+              @php(printDots(get_field('empathy')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Expression</label>
+            <div class="dots">
+              @php(printDots(get_field('expression')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Intimidation</label>
+            <div class="dots">
+              @php(printDots(get_field('intimidation')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Persuasion</label>
+            <div class="dots">
+              @php(printDots(get_field('persuasion')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Socialize</label>
+            <div class="dots">
+              @php(printDots(get_field('socialize')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Streetwise</label>
+            <div class="dots">
+              @php(printDots(get_field('streetwise')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Subterfuge</label>
+            <div class="dots">
+              @php(printDots(get_field('subterfuge')))
+            </div>
+          </div>
         </div>
         <div class="col-md-4 col-xs-12">
           <h3>Merits</h3>
