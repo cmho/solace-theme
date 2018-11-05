90a3427023c550a7f3beaf6676ea7c0656725d11
Carly Ho <yaysunshine@gmail.com>
2018-11-04 18:04:54 -0600

    add more form fields

--- resources/views/partials/content-basics.blade.php
+++ resources/views/partials/content-basics.blade.php
@@ -3,29 +3,38 @@
     <div class="row">
       <div class="col-md-4 col-sm-6 col-xs-12 center">
         <div class="form-row">
-          <label>Character Name</label>
+          <label for="post_title">Character Name</label>
           <input type="text" name="post_title" value="" />
         </div>
         <div class="form-row">
-          <label>Family</label>
+          <label for="family">Family</label>
           <select name="family">
+            @php(print_r(get_field_object('family')))
             @foreach(get_field_object('family')['choices'] as $value=>$label)
               <option value="{{ $value }}">{{ $label }}</option>
             @endforeach
           </select>
-        @if(!get_field('is_npc'))
-          <p class="played-by">Played by {{ get_the_author() }}</p>
+        </div>
+        @if($is_admin)
+          <div class="form-row">
+            <label for="is_npc"><input type="checkbox" name="is_npc" value="true" /> Is NPC?</label>
+          </div>
+          <div class="form-row">
+            <label for="is_secret"><input type="checkbox" name="is_secret" value="true" /> Is Secret?</label>
+          </div>
         @endif
       </div>
       <div class="col-md-8 col-sm-6 col-xs-12">
         <div class="box">
           <div class="content">
-            @if(get_field('quote'))
-              <p class="quote">{{ get_field('quote') }}</p>
-            @endif
-            @if(get_field('public_blurb'))
-              {!! get_field('public_blurb') !!}
-            @endif
+            <div class="form-row">
+              <label for="quote">Quote</label>
+              <input type="text" name="quote" value="" />
+            </div>
+            <div class="form-row">
+              <label for="public_blurb">Public Blurb</label>
+              <textarea name="public_blurb" rows="8"></textarea>
+            </div>
             <dl>
               <dt>Status</dt>
               <dd>{{ get_field('status') }}</dd>
