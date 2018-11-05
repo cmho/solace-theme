e557d2e0f5af6732cca932ac3c96e219387bf85a
Carly Ho <yaysunshine@gmail.com>
2018-11-04 21:05:29 -0600

    add character sheet wrapper

--- resources/views/template-character-creation.blade.php
+++ resources/views/template-character-creation.blade.php
@@ -10,23 +10,25 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    <section class="grey">
-      <div class="wrapper">
-        <div class="row center-xs">
-          <div class="col-xs-12">
-            @include('partials.page-header')
+    <div id="character-sheet">
+      <section class="grey">
+        <div class="wrapper">
+          <div class="row center-xs">
+            <div class="col-xs-12">
+              @include('partials.page-header')
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-    @if($_GET['method'] == 'wizard')
+      </section>
+      @if($_GET['method'] == 'wizard')
 
-    @else
-      <form action="{{ esc_url( admin_url('admin-post.php') ) }}" method="POST">
-        @include('partials.content-basics')
-        <input type="hidden" name="action" value="new_character" />
-        <input type="submit" value="Save Character" />
-      </form>
-    @endif
+      @else
+        <form action="{{ esc_url( admin_url('admin-post.php') ) }}" method="POST">
+          @include('partials.content-basics')
+          <input type="hidden" name="action" value="new_character" />
+          <input type="submit" value="Save Character" />
+        </form>
+      @endif
+    </div>
   @endwhile
 @endsection
