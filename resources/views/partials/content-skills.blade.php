71b4e607124a7c3646328825f61437304ac3c494
Carly Ho <yaysunshine@gmail.com>
2018-11-05 00:58:45 -0600

    add fields for skills

--- resources/views/partials/content-skills.blade.php
+++ resources/views/partials/content-skills.blade.php
@@ -4,48 +4,56 @@
   <label>Academics</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('academics')))
+    <input type="hidden" name="academics" value="{{ get_field('academics') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Computer</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('computer')))
+    <input type="hidden" name="computer" value="{{ get_field('computer') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Crafts</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('crafts')))
+    <input type="hidden" name="crafts" value="{{ get_field('crafts') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Investigation</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('investigation')))
+    <input type="hidden" name="investigation" value="{{ get_field('investigation') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Medicine</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('medicine')))
+    <input type="hidden" name="medicine" value="{{ get_field('medicine') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Occult</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('occult')))
+    <input type="hidden" name="occult" value="{{ get_field('occult') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Politics</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('politics')))
+    <input type="hidden" name="politics" value="{{ get_field('politics') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Science</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('science')))
+    <input type="hidden" name="science" value="{{ get_field('science') }}" />
   </div>
 </div>
 <h4>Physical</h4>
@@ -53,48 +61,56 @@
   <label>Athletics</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('athletics')))
+    <input type="hidden" name="athletics" value="{{ get_field('athletics') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Brawl</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('brawl')))
+    <input type="hidden" name="brawl" value="{{ get_field('brawl') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Drive</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('drive')))
+    <input type="hidden" name="drive" value="{{ get_field('drive') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Firearms</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('firearms')))
+    <input type="hidden" name="firearms" value="{{ get_field('firearms') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Larceny</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('larceny')))
+    <input type="hidden" name="larceny" value="{{ get_field('larceny') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Stealth</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('stealth')))
+    <input type="hidden" name="stealth" value="{{ get_field('stealth') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Survival</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('survival')))
+    <input type="hidden" name="survival" value="{{ get_field('survival') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Weaponry</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('weaponry')))
+    <input type="hidden" name="weaponry" value="{{ get_field('weaponry') }}" />
   </div>
 </div>
 <h4>Social</h4>
@@ -102,47 +118,55 @@
   <label>Animal Ken</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('animal_ken')))
+    <input type="hidden" name="animal_ken" value="{{ get_field('animal_ken') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Empathy</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('empathy')))
+    <input type="hidden" name="empathy" value="{{ get_field('empathy') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Expression</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('expression')))
+    <input type="hidden" name="expression" value="{{ get_field('expression') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Intimidation</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('intimidation')))
+    <input type="hidden" name="intimidation" value="{{ get_field('intimidation') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Persuasion</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('persuasion')))
+    <input type="hidden" name="persuasion" value="{{ get_field('persuasion') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Socialize</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('socialize')))
+    <input type="hidden" name="socialize" value="{{ get_field('socialize') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Streetwise</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('streetwise')))
+    <input type="hidden" name="streetwise" value="{{ get_field('streetwise') }}" />
   </div>
 </div>
 <div class="row between-xs middle-xs">
   <label>Subterfuge</label>
   <div class="dots">
     @php(App\Character::printDots(get_field('subterfuge')))
+    <input type="hidden" name="subterfuge" value="{{ get_field('subterfuge') }}" />
   </div>
 </div>
