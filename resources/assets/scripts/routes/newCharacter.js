73a97279e5eb429d579ee752ed53bdbe4f86090b
Carly Ho <yaysunshine@gmail.com>
2018-11-04 21:48:42 -0600

    implement updating of inputs

--- resources/assets/scripts/routes/newCharacter.js
+++ resources/assets/scripts/routes/newCharacter.js
@@ -5,9 +5,13 @@ export default {
       if ($(this).hasClass('far')) {
         $(this).addClass('fas').removeClass('far');
         $(this).prevAll('i.fa-circle').removeClass('far').addClass('fas');
-      } else {
+      } else if ($(this).hasClass('fas') && $(this).nextAll('.fas').length == 0) {
         $(this).removeClass('fas').addClass('far');
+      } else {
+        $(this).nextAll('.fas').removeClass('fas').addClass('far');
       }
+      var count = $(this).parent('.dots').find('.fas').length;
+      $(this).parent('.dots').find('input').val(count);
     });
   },
   finalize() {
--- resources/views/partials/content-attributes.blade.php
+++ resources/views/partials/content-attributes.blade.php
@@ -8,18 +8,21 @@
       <label>Intelligence</label>
       <div class="dots">
         @php(App\Character::printDots(1))
+        <input type="hidden" value="1" name="intelligence" />
       </div>
     </div>
     <div class="row between-xs middle-xs">
       <label>Wits</label>
       <div class="dots">
         @php(App\Character::printDots(1))
+        <input type="hidden" value="1" name="wits" />
       </div>
     </div>
     <div class="row between-xs middle-xs">
       <label>Resolve</label>
       <div class="dots">
         @php(App\Character::printDots(1))
+        <input type="hidden" value="1" name="resolve" />
       </div>
     </div>
   </div>
@@ -29,18 +32,21 @@
       <label>Strength</label>
       <div class="dots">
         @php(App\Character::printDots(1))
+        <input type="hidden" value="1" name="strength" />
       </div>
     </div>
     <div class="row between-xs middle-xs">
       <label>Dexterity</label>
       <div class="dots">
         @php(App\Character::printDots(1))
+        <input type="hidden" value="1" name="dexterity" />
       </div>
     </div>
     <div class="row between-xs middle-xs">
       <label>Stamina</label>
       <div class="dots">
         @php(App\Character::printDots(1))
+        <input type="hidden" value="1" name="stamina" />
       </div>
     </div>
   </div>
@@ -50,18 +56,21 @@
       <label>Presence</label>
       <div class="dots">
         @php(App\Character::printDots(1))
+        <input type="hidden" value="1" name="presence" />
       </div>
     </div>
     <div class="row between-xs middle-xs">
       <label>Manipulation</label>
       <div class="dots">
         @php(App\Character::printDots(1))
+        <input type="hidden" value="1" name="manipulation" />
       </div>
     </div>
     <div class="row between-xs middle-xs">
       <label>Composure</label>
       <div class="dots">
         @php(App\Character::printDots(1))
+        <input type="hidden" value="1" name="composure" />
       </div>
     </div>
   </div>
