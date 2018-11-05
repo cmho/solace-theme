f1682a21254824135d11591a6ea07c6ae80d4fce
Carly Ho <yaysunshine@gmail.com>
2018-11-04 18:27:46 -0600

    Fix the new character script

--- resources/assets/scripts/routes/newCharacter.js
+++ resources/assets/scripts/routes/newCharacter.js
@@ -1,6 +1,7 @@
 export default {
   init() {
     $('.dots i.fa-circle').on('click', function() {
+      $(this).nextAll('i.fa-circle').removeClass('fas').addClass('far');
       if ($(this).hasClass('far')) {
         $(this).addClass('fas').removeClass('far');
         $(this).prevAll('i.fa-circle').removeClass('far').addClass('fas');
