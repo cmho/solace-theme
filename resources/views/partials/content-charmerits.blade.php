484e841ac92f88f1be72ccb7edde1e3da67844ce
Carly Ho <yaysunshine@gmail.com>
2018-11-05 00:30:12 -0600

    display dot ratings

--- resources/views/partials/content-charmerits.blade.php
+++ resources/views/partials/content-charmerits.blade.php
@@ -1,10 +1,16 @@
+@php
+  global $post;
+@endphp
+
 <h3>Merits</h3>
 <select id="merits">
   @foreach(App\Merits::listGrouped() as $group=>$merits)
     <optgroup label="{{ $group }}">
-      @foreach($merits as $merit)
-        <option value="{{ $merit->ID }}" data-ratings="{{ get_field('allowed_ratings', $merit) }}" data-specification="{{ get_field('requires_specification', $merit) }}" data-description="{{ get_field('requires_description') }}">{{ get_the_title($merit) }}</option>
+      @foreach($merits as $post)
+        @php(setup_postdata($post))
+        <option value="{{ get_the_ID() }}" data-ratings="{{ get_field('allowed_ratings') }}" data-specification="{{ get_field('requires_specification') }}" data-description="{{ get_field('requires_description') }}">{{ get_the_title() }} ({{ App\Merit::dots() }})</option>
       @endforeach
+      @php(wp_reset_postdata())
     </optgroup>
   @endforeach
 </select>
