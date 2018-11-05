cd76067af35923471c915ba3a8680dda001e6f73
Carly Ho <yaysunshine@gmail.com>
2018-11-04 20:47:05 -0600

    fix error

--- resources/views/partials/content-single-character.blade.php
+++ resources/views/partials/content-single-character.blade.php
@@ -99,4 +99,219 @@
         </div>
       </div>
     </div>
+    <div class="row">
+        <div class="col-md-4 col-xs-12">
+          <h3>Skills</h3>
+          <h4>Mental</h4>
+          <div class="row between-xs middle-xs">
+            <label>Academics</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('academics')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Computer</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('computer')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Crafts</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('crafts')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Investigation</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('investigation')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Medicine</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('medicine')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Occult</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('occult')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Politics</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('politics')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Science</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('science')))
+            </div>
+          </div>
+          <h4>Physical</h4>
+          <div class="row between-xs middle-xs">
+            <label>Athletics</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('athletics')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Brawl</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('brawl')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Drive</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('drive')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Firearms</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('firearms')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Larceny</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('larceny')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Stealth</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('stealth')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Survival</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('survival')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Weaponry</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('weaponry')))
+            </div>
+          </div>
+          <h4>Social</h4>
+          <div class="row between-xs middle-xs">
+            <label>Animal Ken</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('animal_ken')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Empathy</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('empathy')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Expression</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('expression')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Intimidation</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('intimidation')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Persuasion</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('persuasion')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Socialize</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('socialize')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Streetwise</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('streetwise')))
+            </div>
+          </div>
+          <div class="row between-xs middle-xs">
+            <label>Subterfuge</label>
+            <div class="dots">
+              @php(App\Character::printDots(get_field('subterfuge')))
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-xs-12">
+          <h3>Merits</h3>
+          <ul class="merits">
+            @foreach(get_field('merits') as $merit)
+              <li>{{ get_the_title($merit['merit']) }}{{ get_field('requires_specification', $merit['merit']) ? " (".$merit['specification'].")" : '' }}{{ count(get_field('allowed_ratings', $merit['merit'])) > 1 ? " ".$merit['rating'] : '' }}{{ $merit['description'] ? '<div>'.$merit['description'].'</div>' : '' }}</li>
+            @endforeach
+        </div>
+        <div class="col-md-4 col-xs-12">
+          <h3>Health</h3>
+          <div class="dots center">
+            @php(App\Character::printSquares(get_field('stamina')+get_field('size')))
+          </div>
+
+          <h3>Willpower</h3>
+          <div class="dots center">
+            @php(App\Character::printSquares(get_field('composure')+get_field('resolve')))
+          </div>
+
+          <h3>Integrity</h3>
+          <div class="dots center">
+            @php(App\Character::printDotsTen(get_field('integrity')))
+          </div>
+
+          <dl>
+            <dt>Size</dt>
+            <dd>{{ get_field('size') }}</dd>
+            <dt>Speed</dt>
+            <dd>{{ get_field('strength')+get_field('dexterity')+5 }}</dd>
+            <dt>Defense</dt>
+            <dd>{{ min(get_field('wits'), get_field('dexterity'))+get_field('athletics') }}</dd>
+            <dt>Armor</dt>
+            <dd>{{ get_field('armor') }}</dd>
+            <dt>Initiative Mod</dt>
+            <dd>{{ get_field('dexterity')+get_field('composure') }}</dd>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="questionnaire" class="yellow">
+  <div class="wrapper">
+    <div class="row">
+      <div class="col-xs-12">
+        <h3>Questionnaire</h3>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="experience" class="grey">
+  <div class="wrapper">
+    <div class="row">
+      <div class="col-xs-12">
+        <h3>Experience</h3>
+        <ul>
+          @foreach(App\Character::experienceRecords() as $post)
+            @php(setup_postdata($post))
+            <li>{{ get_the_title() }}: {{ get_field('amount') }}</li>
+          @endforeach
+          @php(wp_reset_postdata())
+        </ul>
+        <p><strong>Current Total:</strong> {{ App\Character::sumExperience() }}</p>
+      </div>
+    </div>
+  </div>
 </section>
