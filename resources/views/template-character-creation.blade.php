13d9aaa675c323a3b0b1d6208139019de63026f3
Carly Ho <yaysunshine@gmail.com>
2018-11-04 21:40:25 -0600

    add questionnaire partial

--- resources/views/template-character-creation.blade.php
+++ resources/views/template-character-creation.blade.php
@@ -26,12 +26,21 @@
       @else
         <form action="{{ esc_url( admin_url('admin-post.php') ) }}" method="POST">
           <section id="character-sheet" class="grey">
-            @include('partials.content-basics')
-            @include('partials.content-attributes')
-            @include('partials.content-skills-merits-misc')
+            <div class="wrapper">
+              @include('partials.content-basics')
+              @include('partials.content-attributes')
+              @include('partials.content-skills-merits-misc')
+            </div>
           </section>
           <section id="questionnaire" class="yellow">
-
+            <div class="wrapper">
+              <div class="row">
+                <div class="col-xs-12">
+                  <h3>Questionnaire</h3>
+                  @include('partials.content-questionnaire')
+                </div>
+              </div>
+            </div>
           </section>
           <section id="controls" class="grey">
             <div class="wrapper">
