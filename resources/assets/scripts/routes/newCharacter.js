ff8af6c8a9ad48a9cda166336a4039e41fa1e463
Carly Ho <cho@cliquestudios.com>
2018-11-05 17:57:25 -0600

    add scripting for adding merits to sheet

--- resources/assets/scripts/routes/newCharacter.js
+++ resources/assets/scripts/routes/newCharacter.js
@@ -1,5 +1,7 @@
 export default {
   init() {
+    var ajaxurl;
+
     $('.dots i.fa-circle').on('click', function() {
       $(this).nextAll('i.fa-circle').removeClass('fas').addClass('far');
       if ($(this).hasClass('far')) {
@@ -77,6 +79,21 @@ export default {
 
     $('input[name="athletics"]').on('change', function() {
       updateDefense();
+    });
+
+    $('#add-merit').on('click', function() {
+      var merit = parseInt($('#merits option:selected').first().val());
+      $.ajax({
+        url: ajaxurl,
+        method: 'POST',
+        data: {
+          id: merit,
+        },
+        success: function(data) {
+          var newItem = "<li>"+data.name+" <i class='fas fa-pencil'></i><i class='fas fa-trash'></i><input type='hidden' name='merits[][\"merit\"]' value='"+data.id+"' /><input type='hidden' name='merits[][\"rating\"]' value='"+data.ratings[0]+"' /><input type='hidden' name='merits[][\"specification\"]' value='' /><input type='hidden' name='merits[][\"description\"]' value='' /></li>";
+          $('ul.merits').append(newItem);
+        }
+      })
     });
   },
   finalize() {
