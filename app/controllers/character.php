b3591bc12610de0d68af4fe152d3a2d49f24f094
Carly Ho <yaysunshine@gmail.com>
2018-11-04 20:54:12 -0600

    check  value

--- app/controllers/character.php
+++ app/controllers/character.php
@@ -41,7 +41,7 @@ class Character extends Controller
         foreach ($exp as $e) {
             $sum += get_field('amount', $e);
         }
-        return print_r($exp);
+        return $character;
     }
 
     public static function printDots($amt = 0)
