bc5ba00efa5ed649ec8f54903db4747221013227
Carly Ho <cho@cliquestudios.com>
2018-11-07 11:09:52 -0600

    adjust looping

--- resources/views/partials/content-custom.blade.php
+++ resources/views/partials/content-custom.blade.php
@@ -1,7 +1,8 @@
+@php
+  $count = 1;
+@endphp
+
 @if(have_rows('sections'))
-  @php
-    $count = 1;
-  @endphp
   @while(have_rows('sections'))
     @php(the_row())
     @if(get_row_layout() == 'single_text_column')
@@ -72,8 +73,6 @@
         </div>
       </section>
     @endif
-    @php
-      $count++;
-    @endphp
+    @php($count++;)
   @endwhile
 @endif
