4a3bce558b0350b6e7f77ceda438261bb0589653
Carly Ho <cho@cliquestudios.com>
2018-11-07 11:02:13 -0600

    test test test

--- resources/views/partials/content-custom.blade.php
+++ resources/views/partials/content-custom.blade.php
@@ -1,12 +1,1 @@
-@if(have_rows('sections'))
-  @php
-    $count = 1;
-  @endphp
-  @while(have_rows('sections'))
-    @php(the_row())
-    
-    @php
-      $count++;
-    @endphp
-  @endwhile
-@endif
+test
