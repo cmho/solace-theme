b31c19cf14c68bfc71d7bb74852c6930fb150221
Carly Ho <cho@cliquestudios.com>
2018-11-07 16:38:33 -0600

    fancy up 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,12 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+  <section class="grey">
+    <div class="wrapper">
+      <div class="row center-xs">
+        <div class="col-xs-12">
+          @include('partials.page-header')
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="content">
+              @if (!have_posts())
+                <div class="alert alert-warning">
+                  {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+                </div>
+                {!! get_search_form(false) !!}
+              @endif
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    {!! get_search_form(false) !!}
-  @endif
+  </section>
 @endsection
