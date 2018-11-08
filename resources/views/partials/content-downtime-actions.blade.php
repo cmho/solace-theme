000da08c0be0a7711172bf873dbd80e09106b21f
Carly Ho <yaysunshine@gmail.com>
2018-11-07 21:33:07 -0600

    more debugging

--- resources/views/partials/content-downtime-actions.blade.php
+++ resources/views/partials/content-downtime-actions.blade.php
@@ -39,5 +39,37 @@
     @endforeach
   @endforeach
 @else
-  B
+  @if(App\App::isAdmin() && $_GET['game'] != null)
+      @php
+        $game = get_post($_GET['game'])
+      @endphp
+      <h3>Downtimes for {{ get_the_title($game) }}</h3>
+      @if(App\Downtimes::listDowntimesForGame($_GET['game']))
+          @foreach(App\Downtimes::listDowntimesForGame($_GET['game']) as $character=>$downtimes)
+              @php($character = get_post($character->ID))
+              <h4>{!! get_the_title($character) !!}</h4>
+              @foreach($downtimes as $downtime)
+                  <div class="downtime">
+                      <h4>{{ get_the_title($downtime) }}</h4>
+                      <p class="assets"><strong>Assets:</strong> {{ get_field('assets', $downtime) }}</p>
+                      <p class="goal"><strong>Goal:</strong> {{ get_field('goal', $downtime) }}</p>
+                      <div class="description">
+                          {!! get_field('description', $downtime) !!}
+                      </div>
+                      @if(get_field('response', $downtime))
+                          <hr />
+                          <div class="response">
+                              {!! get_field('response', $downtime) !!}
+                          </div>
+                      @endif
+                      <a href="{{ get_the_permalink($downtime) }}/?mode=respond">{{ (get_field('response', $downtime)) ? 'Edit Response' : 'Respond' }}</a>
+                  </div>
+              @endforeach
+          @endforeach
+      @else
+          <p style="text-align: center">No downtimes for this game yet.</p>
+      @endif
+  @else
+    no game specified
+  @endif
 @endif
