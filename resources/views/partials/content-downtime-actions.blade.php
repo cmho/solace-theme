10d393dcd7a036e3f92bf2322ad708dd76f59197
Carly Ho <yaysunshine@gmail.com>
2018-11-07 22:40:36 -0600

    show 'no downtimes for this game' when none returned

--- resources/views/partials/content-downtime-actions.blade.php
+++ resources/views/partials/content-downtime-actions.blade.php
@@ -25,23 +25,27 @@
   @if(date('Ymd') >= get_field('downtimes_open', $game) && date('Ymd') <= get_field('downtimes_close', $game))
       <p class="downtime-status">Downtimes are currently <strong>open</strong>. They will close at 11:59 PM on {{ date('m/d/y', strtotime(get_field('downtimes_close', $game))) }}.</p>
   @endif
-  @foreach($downtimes as $downtime)
-    <div class="downtime">
-      <h4>{{ $downtime->post_title }}</h4>
-      <p class="assets"><strong>Assets:</strong> {{ get_field('assets', $downtime) }}</p>
-      <p class="goal"><strong>Goal:</strong> {{ get_field('goal', $downtime) }}</p>
-      <div class="description">
-        {!! get_field('description', $downtime) !!}
-      </div>
-      @if(get_field('response', $downtime))
-        <hr />
-        <div class="response">
-            {!! get_field('response', $downtime) !!}
+  @if($downtimes)
+    @foreach($downtimes as $downtime)
+      <div class="downtime">
+        <h4>{{ $downtime->post_title }}</h4>
+        <p class="assets"><strong>Assets:</strong> {{ get_field('assets', $downtime) }}</p>
+        <p class="goal"><strong>Goal:</strong> {{ get_field('goal', $downtime) }}</p>
+        <div class="description">
+          {!! get_field('description', $downtime) !!}
         </div>
-      @endif
-      @if(App\App::isAdmin())
-        <a href="{{ get_the_permalink($downtime) }}/?mode=respond">{{ (get_field('response', $downtime)) ? 'Edit Response' : 'Respond' }}</a>
-      @endif
-    </div>
-  @endforeach
+        @if(get_field('response', $downtime))
+          <hr />
+          <div class="response">
+              {!! get_field('response', $downtime) !!}
+          </div>
+        @endif
+        @if(App\App::isAdmin())
+          <a href="{{ get_the_permalink($downtime) }}/?mode=respond">{{ (get_field('response', $downtime)) ? 'Edit Response' : 'Respond' }}</a>
+        @endif
+      </div>
+    @endforeach
+  @else
+    <p><em>No downtimes for this game.</em></p>
+  @endif
 @endforeach
