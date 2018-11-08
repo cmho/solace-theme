c1b683a8a5182254eb5291e69373efdf8ceba5db
Carly Ho <yaysunshine@gmail.com>
2018-11-07 23:54:53 -0600

    add new files

---
+++ resources/views/template-new-downtime.blade.php
@@ -0,0 +1,31 @@
+{{--
+  Template Name: New Downtime Action Template
+--}}
+
+@php
+  $user = wp_get_current_user();
+  if (!$user) {
+    header('Location:'.home_url('/'));
+  }
+@endphp
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    <section class="grey">
+      <div class="wrapper">
+        <div class="row center-xs">
+          <div class="col-xs-12">
+            @include('partials.page-header')
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12">
+            @include('partials.content-new-downtime-action')
+          </div>
+        </div>
+      </div>
+    </section>
+  @endwhile
+@endsection
