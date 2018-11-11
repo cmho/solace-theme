99355435e3bb8dc2ea20a31718ea8347cce1d732
Carly Ho <yaysunshine@gmail.com>
2018-11-11 17:29:06 -0600

    fix namespacing

--- app/controllers/experience.php
+++ app/controllers/experience.php
@@ -16,16 +16,16 @@ class Experience extends Controller
             'meta_query' => array(
                 array(
                     'key' => 'experience_expenditure',
-                    'value' => get_the_ID()
+                    'value' => \get_the_ID()
                 )
             )
         ));
-        return wp_get_revision_ui_diff($character->post_parent, wp_get_post_revision($character), $character->ID);
+        return \wp_get_revision_ui_diff($character->post_parent, \wp_get_post_revision($character), $character->ID);
     }
 
     public static function getcharacter()
     {
-        $character = get_posts(array(
+        $character = \get_posts(array(
             'post_type' => 'character',
             'posts_per_page' => -1,
             'orderby' => 'date_modified',
@@ -37,6 +37,6 @@ class Experience extends Controller
                 )
             )
         ));
-        return get_post($character->post_parent);
+        return \get_post($character->post_parent);
     }
 }
