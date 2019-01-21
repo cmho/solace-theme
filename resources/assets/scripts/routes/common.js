eef905cee1e0340a0312bd4c836b15e65eb8509c
Carly Ho <cho@cliquestudios.com>
2019-01-21 11:21:28 -0600

    fix field reference for real

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -180,7 +180,7 @@ export default {
     });
 
     $('#add-specialty').on('click', function() {
-      var skill = $('#skill option:selected').val();
+      var skill = $('#skills_list option:selected').val();
       var specialty = $('#specialty_name').val();
       var num = $('.skill-specialties li').length;
       var item = '<li><strong class="skill">'+skill+':</strong> <span class="specialty">'+specialty+'</span> <button type="button" class="delete"><i class="fas fa-trash"></i></button><input type="hidden" name="skill_specialties_'+num+'_skill" value="'+skill+'" /><input type="hidden" name="skill_specialties_'+num+'_specialty" value="'+specialty+'" /></li>';
