7a0ffefe9516a28ced8b135c29cf9584af5d55cd
Carly Ho <yaysunshine@gmail.com>
2019-01-21 23:36:08 -0600

    add button to pre-existing entries

--- resources/views/partials/content-conditions.blade.php
+++ resources/views/partials/content-conditions.blade.php
@@ -12,7 +12,7 @@
 <ul class="conditions">
   @if(get_field('conditions'))
     @foreach(get_field('conditions') as $i=>$condition)
-    <li><strong>{{ get_the_title($condition['condition']->ID) }}</strong><br />
+    <li><strong>{{ get_the_title($condition['condition']->ID) }}</strong> <button class="delete" type="button"><i class="fas fa-trash"></i><span class="sr-only">Delete</span></button><br />
       {{ $condition['note'] }}
       <input type="hidden" name="conditions_{{ $i }}_condition" value="{{ $condition['condition']->ID }}" />
       <input type="hidden" name="conditions_{{ $i }}_note" value="{{ $condition['note'] }}" />
