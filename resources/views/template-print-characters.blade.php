ba197d2d8a073f7aabaffa81f87ef29a946dc0b4
Carly Ho <yaysunshine@gmail.com>
2019-01-27 21:30:02 -0600

    update print styles

--- resources/views/template-print-characters.blade.php
+++ resources/views/template-print-characters.blade.php
@@ -5,7 +5,11 @@
 @extends('layouts.print')
 
 @section('content')
-  @foreach(App\Characters::activeList() as $char)
-    @include('partials.content-single-character')
+  @foreach(App\Characters::activeList() as $post)
+    @php(setup_postdata($post))
+    <article>
+      @include('partials.content-single-character')
+    </article>
   @endforeach
+  @php(wp_reset_postdata())
 @endsection
