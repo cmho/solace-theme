2edaefde998dc0931c12d623ce6066d1fcd4eaf3
Carly Ho <yaysunshine@gmail.com>
2019-01-30 21:02:58 -0600

    update event templates

--- resources/views/partials/content-single-event.blade.php
+++ resources/views/partials/content-single-event.blade.php
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-md-4 col-xs-12">
         <h2>{{ get_the_title() }}</h2>
+        <p class="date">{{ get_field('date') }}</p>
       </div>
       <div class="col-md-8 col-xs-12">
         <div class="box">
