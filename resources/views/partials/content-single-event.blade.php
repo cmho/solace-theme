e2e9e85dc0d32db2c10eef3bf9fc2ff70ae0bdeb
Carly Ho <yaysunshine@gmail.com>
2019-01-30 20:58:32 -0600

    add template for event pages

---
+++ resources/views/partials/content-single-event.blade.php
@@ -0,0 +1,16 @@
+<section class="grey">
+  <div class="wrapper">
+    <div class="row">
+      <div class="col-md-4 col-xs-12">
+        <h2>{{ get_the_title() }}</h2>
+      </div>
+      <div class="col-md-8 col-xs-12">
+        <div class="box">
+          <div class="content">
+            @php(the_content())
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
