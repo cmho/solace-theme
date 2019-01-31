74be1ac981d7395c92ec7852847717a611cf64bd
Carly Ho <yaysunshine@gmail.com>
2019-01-30 23:03:05 -0600

    fix repeated questions

--- resources/views/partials/content-questionnaire.blade.php
+++ resources/views/partials/content-questionnaire.blade.php
@@ -1,44 +1,42 @@
-@foreach(App\Character::questionnaire() as $q)
-  <div class="form-row">
-    <label for="backstory">Character Backstory</label>
-    <p class="help">500 words or less; timelines or bullet points are acceptable and encouraged.</p>
-    <textarea name="backstory" rows="6">{!! get_field('backstory') !!}</textarea>
-  </div>
-  <div class="form-row">
-    <label for="connections">List at least 3 connections to other characters, PCs or NPCs.</label>
-    <p class="help">Please list character name, player name (if applicable), and a brief description of the nature of that connection.</p>
-    <textarea name="connections" rows="6">{!! get_field('connections') !!}</textarea>
-  </div>
-  <div class="form-row">
-    <label for="complications">Give a brief list of complicating factors (vices, unfinished business, enemies) in your character's life.</label>
-    <textarea name="complications" rows="6">{!! get_field('complications') !!}</textarea>
-  </div>
-  <div class="form-row">
-    <label for="supernatural">What was your first experience with the supernatural? Which supernatural experience most shaped your worldview?</label>
-    <textarea name="supernatural" rows="6">{!! get_field('supernatural') !!}</textarea>
-  </div>
-  <div class="form-row">
-    <label for="massacre">Where were you when the massacre took place? Who did you lose?</label>
-    <textarea name="massacre" rows="6">{!! get_field('massacre') !!}</textarea>
-  </div>
-  <div class="form-row">
-    <label for="survive">What is the worst thing you've done to survive?</label>
-    <textarea name="survive" rows="6">{!! get_field('survive') !!}</textarea>
-  </div>
-  <div class="form-row">
-    <label for="loss">What loss have you never gotten over, or that hurt the most?</label>
-    <textarea name="loss" rows="6">{!! get_field('loss') !!}</textarea>
-  </div>
-  <div class="form-row">
-    <label for="hobbies">What's one thing you hold onto outside of your hunter life?</label>
-    <textarea name="hobbies" rows="6">{!! get_field('hobbies') !!}</textarea>
-  </div>
-  <div class="form-row">
-    <label for="coping">What do you do to cope with the stresses of this life?</label>
-    <textarea name="coping" rows="6">{!! get_field('coping') !!}</textarea>
-  </div>
-  <div class="form-row">
-    <label for="anything_else">Is there anything else the Storytellers should know about this character?</label>
-    <textarea name="anything_else" rows="6">{!! get_field('anything_else') !!}</textarea>
-  </div>
-@endforeach
+<div class="form-row">
+  <label for="backstory">Character Backstory</label>
+  <p class="help">500 words or less; timelines or bullet points are acceptable and encouraged.</p>
+  <textarea name="backstory" rows="6">{!! get_field('backstory') !!}</textarea>
+</div>
+<div class="form-row">
+  <label for="connections">List at least 3 connections to other characters, PCs or NPCs.</label>
+  <p class="help">Please list character name, player name (if applicable), and a brief description of the nature of that connection.</p>
+  <textarea name="connections" rows="6">{!! get_field('connections') !!}</textarea>
+</div>
+<div class="form-row">
+  <label for="complications">Give a brief list of complicating factors (vices, unfinished business, enemies) in your character's life.</label>
+  <textarea name="complications" rows="6">{!! get_field('complications') !!}</textarea>
+</div>
+<div class="form-row">
+  <label for="supernatural">What was your first experience with the supernatural? Which supernatural experience most shaped your worldview?</label>
+  <textarea name="supernatural" rows="6">{!! get_field('supernatural') !!}</textarea>
+</div>
+<div class="form-row">
+  <label for="massacre">Where were you when the massacre took place? Who did you lose?</label>
+  <textarea name="massacre" rows="6">{!! get_field('massacre') !!}</textarea>
+</div>
+<div class="form-row">
+  <label for="survive">What is the worst thing you've done to survive?</label>
+  <textarea name="survive" rows="6">{!! get_field('survive') !!}</textarea>
+</div>
+<div class="form-row">
+  <label for="loss">What loss have you never gotten over, or that hurt the most?</label>
+  <textarea name="loss" rows="6">{!! get_field('loss') !!}</textarea>
+</div>
+<div class="form-row">
+  <label for="hobbies">What's one thing you hold onto outside of your hunter life?</label>
+  <textarea name="hobbies" rows="6">{!! get_field('hobbies') !!}</textarea>
+</div>
+<div class="form-row">
+  <label for="coping">What do you do to cope with the stresses of this life?</label>
+  <textarea name="coping" rows="6">{!! get_field('coping') !!}</textarea>
+</div>
+<div class="form-row">
+  <label for="anything_else">Is there anything else the Storytellers should know about this character?</label>
+  <textarea name="anything_else" rows="6">{!! get_field('anything_else') !!}</textarea>
+</div>
