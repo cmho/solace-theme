d070cf71c0622f20d1f525be333d28dd41cdd54b
Carly Ho <yaysunshine@gmail.com>
2019-02-08 14:37:29 -0600

    add theme color meta tag

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="theme-color" content="#333333" />
   <script type="text/javascript">
     var ajaxurl = "{{ admin_url('admin-ajax.php') }}";
   </script>
