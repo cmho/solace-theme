ce5dc5645757a48754c7db7109ba59ab898a52a9
Carly Ho <cho@cliquestudios.com>
2019-02-08 11:37:49 -0600

    styles for the style god

--- resources/views/template-dashboard.blade.php
+++ resources/views/template-dashboard.blade.php
@@ -29,41 +29,43 @@
             @php(setup_postdata($post))
             <li data-character="{{ $post->ID }}">
               <h3><a href="#">@php(the_title())</a></h3>
-              <div class="health" data-health="{{ get_field('current_health') }}">
-                <h4>Health</h4>
-                {{ App\Character::printSquaresInteractable(get_field('current_health')) }}
-              </div>
-              <div class="willpower" data-willpower="{{ get_field('current_willpower') }}">
-                <h4>Willpower</h4>
-                {{ App\Character::printSquaresInteractable(get_field('current_willpower')) }}
-              </div>
-              <div class="integrity">
-                <h4>Integrity</h4>
-                <span class="current-integrity">{{ get_field('integrity') }}</span> <button type="button" class="button small breaking-point">Breaking Point</button>
-              </div>
-              <div class="conditions">
-                <h4>Conditions</h4>
-                <form>
-                  <label for="conditions_list">Condition</label>
-                  <select id="conditions_list">
-                    @foreach(App\Conditions::list() as $condition)
-                      <option value="{{ $condition->ID }}">{{ get_the_title($condition->ID)}}</option>
-                    @endforeach
-                  </select>
-                  <label for="condition_note">Note</label>
-                  <input type="text" name="condition_note" id="condition_note" />
-                  <button type="button" class="button small" id="add-condition">Add</button>
-                  <input type="hidden" name="conditions" val="{{ count(get_field('conditions')) }}" />
-                </form>
-                @if(get_field('conditions'))
-                  <ul>
-                    @foreach(get_field('conditions') as $condition)
-                      <li><strong>{{ get_the_title($condition['condition']->ID) }}</strong> <button type="button" class="button small resolve-button">Resolve</button><br />
-                        {{ $condition['note'] }}
-                      </li>
-                    @endforeach
-                  </ul>
-                @endif
+              <div class="character-content">
+                <div class="health" data-health="{{ get_field('current_health') }}">
+                  <h4>Health</h4>
+                  {{ App\Character::printSquaresInteractable(get_field('current_health')) }}
+                </div>
+                <div class="willpower" data-willpower="{{ get_field('current_willpower') }}">
+                  <h4>Willpower</h4>
+                  {{ App\Character::printSquaresInteractable(get_field('current_willpower')) }}
+                </div>
+                <div class="integrity">
+                  <h4>Integrity</h4>
+                  <span class="current-integrity">{{ get_field('integrity') }}</span> <button type="button" class="button small breaking-point">Breaking Point</button>
+                </div>
+                <div class="conditions">
+                  <h4>Conditions</h4>
+                  <form>
+                    <label for="conditions_list">Condition</label>
+                    <select id="conditions_list">
+                      @foreach(App\Conditions::list() as $condition)
+                        <option value="{{ $condition->ID }}">{{ get_the_title($condition->ID)}}</option>
+                      @endforeach
+                    </select>
+                    <label for="condition_note">Note</label>
+                    <input type="text" name="condition_note" id="condition_note" />
+                    <button type="button" class="button small" id="add-condition">Add</button>
+                    <input type="hidden" name="conditions" val="{{ count(get_field('conditions')) }}" />
+                  </form>
+                  @if(get_field('conditions'))
+                    <ul>
+                      @foreach(get_field('conditions') as $condition)
+                        <li><strong>{{ get_the_title($condition['condition']->ID) }}</strong> <button type="button" class="button small resolve-button">Resolve</button><br />
+                          {{ $condition['note'] }}
+                        </li>
+                      @endforeach
+                    </ul>
+                  @endif
+                </div>
               </div>
             </li>
           @endforeach
