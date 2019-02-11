24759e3daf5578cd1f74f25a908c0554da849ffc
Carly Ho <cho@cliquestudios.com>
2019-02-11 11:21:45 -0600

    fix square count

--- resources/views/template-dashboard.blade.php
+++ resources/views/template-dashboard.blade.php
@@ -85,13 +85,13 @@
       <div class="character-sheet">
         <h2>{{ get_the_title($char->ID) }}</h2>
         <div class="character-content">
-          <div class="health" data-health="{{ get_field('current_health') }}">
+          <div class="health" data-health="{{ get_field('current_health', $char->ID) }}">
             <h3>Health</h3>
-            {{ App\Character::printSquaresInteractable(get_field('current_health')) }}
+            {{ App\Character::printSquaresInteractable(get_field('current_health', $char->ID)) }}
           </div>
-          <div class="willpower" data-willpower="{{ get_field('current_willpower') }}">
+          <div class="willpower" data-willpower="{{ get_field('current_willpower', $char->ID) }}">
             <h3>Willpower</h3>
-            {{ App\Character::printSquaresInteractable(get_field('current_willpower')) }}
+            {{ App\Character::printSquaresInteractable(get_field('current_willpower', $char->ID)) }}
           </div>
           <h3>Attributes</h3>
           <p><strong>Mental Attributes:</strong> Intelligence {{ get_field('intelligence', $char->ID) }}, Wits {{ get_field('wits', $char->ID) }}, Resolve {{ get_field('resolve', $char->ID) }}</p>
