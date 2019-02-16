561bccce90fbd63993c897206317d3075288c59b
Carly Ho <yaysunshine@gmail.com>
2019-02-16 01:28:15 -0600

    remove require

--- app/filters.php
+++ app/filters.php
@@ -1,8 +1,6 @@
 <?php
 namespace App;
 
-require_once __DIR__.'../vendor/autoload.php';
-
 $config = \HTMLPurifier_Config::createDefault();
 $config->set('HTML.Allowed', 'p,br,b,a[href],i,em,strong,hr');
 $config->set('URI.MakeAbsolute', true);
