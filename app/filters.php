ee230d8448c1fab5723a26fabb3cb7cbbc10f61a
Carly Ho <yaysunshine@gmail.com>
2019-02-20 23:18:22 -0600

    debug

--- app/filters.php
+++ app/filters.php
@@ -602,9 +602,9 @@ function resolveCondition()
     $char = get_post($_POST['character']);
     $conditions = get_field('conditions', $char->ID);
     $cond = array_splice($conditions, intval($_POST['condition']), 1);
+    print_r($cond);
+    die(1);
     if (get_field('resolution', $cond['condition']->ID) && empty($_POST['delete'])) {
-        print_r(get_field('resolution', $cond['condition']->ID));
-        die(1);
         wp_insert_post(array(
             'post_type' => 'beat',
             'post_status' => 'publish',
