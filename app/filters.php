c02504ac52b666e031056e9bb8a9e65d52a5a6cd
Carly Ho <yaysunshine@gmail.com>
2019-02-20 22:53:17 -0600

    fix getting ID possibly?

--- app/filters.php
+++ app/filters.php
@@ -602,7 +602,7 @@ function resolveCondition()
     $char = get_post($_POST['character']);
     $conditions = get_field('conditions', $char->ID);
     $cond = array_splice($conditions, intval($_POST['condition']), 1);
-    if (get_field('resolution', $cond->ID) && !isset($_POST['delete'])) {
+    if (get_field('resolution', $cond['condition']->ID) && !isset($_POST['delete'])) {
         wp_insert_post(array(
             'post_type' => 'beat',
             'post_status' => 'publish',
