1e5dd96aeef910ed966e016d542408ea8941efc1
Carly Ho <cho@cliquestudios.com>
2019-02-22 16:59:24 -0600

    tell merit modal about content source

--- app/controllers/character.php
+++ app/controllers/character.php
@@ -232,7 +232,7 @@ class Character extends Controller
         $merits = get_field('merits', $id);
         $merits = array_map(function ($m) {
             $merit = get_post($m['merit']->ID);
-            return '<a class="js-modal merit-link" href="#" data-id="'.$m['merit']->ID.'">'.get_the_title($merit->ID)."</a> ".$m['rating'].($m['specification'] ? ' ('.$m['specification'].')' : '');
+            return '<a class="js-modal merit-link" href="#" data-id="'.$m['merit']->ID.'" data-content="merits-modal">'.get_the_title($merit->ID)."</a> ".$m['rating'].($m['specification'] ? ' ('.$m['specification'].')' : '');
         }, $merits);
         return join(", ", $merits);
     }
