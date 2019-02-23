cc7c0e8f1f41fbf896be624c5cc504b1a4089726
Carly Ho <yaysunshine@gmail.com>
2019-02-23 14:46:37 -0600

    Add mail template; add mailer for character submissions

--- app/filters.php
+++ app/filters.php
@@ -4,6 +4,155 @@ namespace App;
 use Roots\Sage\Template\Blade;
 use Roots\Sage\Template\BladeProvider;
 
+$mailHeader = `<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"
+ xmlns:v="urn:schemas-microsoft-com:vml"
+ xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+  <!--[if gte mso 9]><xml>
+   <o:OfficeDocumentSettings>
+    <o:AllowPNG/>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+   </o:OfficeDocumentSettings>
+  </xml><![endif]-->
+  <!-- fix outlook zooming on 120 DPI windows devices -->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- So that mobile will display zoomed in -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- enable media queries for windows phone 8 -->
+  <meta name="format-detection" content="date=no"> <!-- disable auto date linking in iOS 7-9 -->
+  <meta name="format-detection" content="telephone=no"> <!-- disable auto telephone linking in iOS 7-9 -->
+  <title>Single Column</title>
+
+  <style type="text/css">
+body {
+  margin: 0;
+  padding: 0;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  color: #fff5a3;
+}
+
+table {
+  border-spacing: 0;
+}
+
+table td {
+  border-collapse: collapse;
+}
+
+.ExternalClass {
+  width: 100%;
+}
+
+.ExternalClass,
+.ExternalClass p,
+.ExternalClass span,
+.ExternalClass font,
+.ExternalClass td,
+.ExternalClass div {
+  line-height: 100%;
+}
+
+.ReadMsgBody {
+  width: 100%;
+  background-color: #333333;
+}
+
+table {
+  mso-table-lspace: 0pt;
+  mso-table-rspace: 0pt;
+}
+
+img {
+  -ms-interpolation-mode: bicubic;
+}
+
+.yshortcuts a {
+  border-bottom: none !important;
+}
+
+@media screen and (max-width: 599px) {
+  .force-row,
+  .container {
+    width: 100% !important;
+    max-width: 100% !important;
+  }
+}
+@media screen and (max-width: 400px) {
+  .container-padding {
+    padding-left: 12px !important;
+    padding-right: 12px !important;
+  }
+}
+.ios-footer a {
+  color: #fff5a3 !important;
+  text-decoration: underline;
+}
+a[href^="x-apple-data-detectors:"],
+a[x-apple-data-detectors] {
+  color: inherit !important;
+  text-decoration: none !important;
+  font-size: inherit !important;
+  font-family: inherit !important;
+  font-weight: inherit !important;
+  line-height: inherit !important;
+}
+</style>
+</head>
+
+<body style="margin:0; padding:0;" bgcolor="#333333" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+
+<!-- 100% background wrapper (grey background) -->
+<table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" bgcolor="#333333">
+  <tr>
+    <td align="center" valign="top" bgcolor="#333333" style="background-color: #333333;">
+
+      <br>
+
+      <!-- 600px container (white background) -->
+      <table border="0" width="600" cellpadding="0" cellspacing="0" class="container" style="width:600px;max-width:600px">
+        <tr>
+          <td class="container-padding header" align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:24px;font-weight:bold;padding-bottom:12px;color:#fff5a3;padding-left:24px;padding-right:24px">
+            SOLACE
+          </td>
+        </tr>
+        <tr>
+          <td class="container-padding content" align="left" style="padding-left:24px;padding-right:24px;padding-top:12px;padding-bottom:12px;background-color:#333333; border: 3px #FFF solid;">
+            <br>
+`;
+
+$mailFooter = `</td>
+        </tr>
+        <tr>
+          <td class="container-padding footer-text" align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:12px;line-height:16px;color:#fff5a3;padding-left:24px;padding-right:24px">
+            <br /><br />
+            <a href="https://solacelarp.com/" style="color:#fff5a3">solacelarp.com</a><br>
+
+            <br><br>
+
+          </td>
+        </tr>
+      </table>
+<!--/600px container -->
+
+
+    </td>
+  </tr>
+</table>
+<!--/100% background wrapper-->
+
+</body>
+</html>
+`;
+
+$titleStart = `<div class="title" style="font-family:Helvetica, Arial, sans-serif;font-size:18px;font-weight:600;color:#fff5a3;">`;
+$titleEnd = `</div>
+<br>`;
+
+$bodyStart = `<div class="body-text" style="font-family:Helvetica, Arial, sans-serif;font-size:14px;line-height:20px;text-align:left;color:#fff5a3">`;
+$bodyEnd = `<br><br>
+</div>`;
+
 /**
  * Add <body> classes
  */
@@ -263,15 +412,29 @@ function update_character()
                 'role' => 'administrator'
             ));
             $message =
-                "There's a new experience expenditure for ".get_post($post)->post_title.". To see and approve it, go here:
-                    <a href='https://solacelarp.com/wp-admin/revision.php?revision=".$post;
+                "There's a new experience expenditure for ".get_post($post)->post_title.". To see and approve it, go
+                    <a href='https://solacelarp.com/wp-admin/revision.php?revision=".$post.">here</a>.";
             foreach ($admins as $admin) {
-                \wp_mail($admin->user_email, '[Solace] New experience expenditure for '.get_post($post)->post_title, $message);
+                \wp_mail($admin->user_email, '[Solace] New experience expenditure for '.get_post($post)->post_title, $mailHeader.$titleStart.'New experience expenditure for '.get_post($post)->post_title.$title_end.$body_start.$message.$body_end.$mailFooter);
             }
             header('Location:'.\get_the_permalink($char));
             die(1);
         } else {
-            $post_content['ID'] = $purifier->purify($_POST['id']);
+            $p = intval($_POST['id']);
+            $po = get_post($p);
+            if (get_field('status', $p) == 'In Progress' && $_POST['status'] == 'Submitted') {
+                $message =
+                "There's a new character submission from ".get_the_author_meta('nickname', $po->post_author).": ".$po->post_title.". To review it, go here:
+                    <a href='".get_the_permalink($p)."'>here</a>";
+                foreach ($admins as $admin) {
+                    \wp_mail(
+                        $admin->user_email,
+                        '[Solace] New character submission from '.get_the_author_meta('nickname', $po->post_author).' :'.$po->post_title,
+                        $mailHeader.$titleStart.'New character submission from '.get_the_author_meta('nickname', $po->post_author).' :'.$po->post_title.$title_end.$body_start.$message.$body_end.$mailFooter
+                    );
+                }
+            }
+            $post_content['ID'] = intval($_POST['id']);
         }
     }
     $post = \wp_insert_post($post_content);
