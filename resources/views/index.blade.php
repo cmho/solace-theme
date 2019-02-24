4e7235a026852b54ef878f4de3e0ffbf71a9d814
Carly Ho <yaysunshine@gmail.com>
2019-02-23 19:19:14 -0600

    improve formatting on blog

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,30 +8,31 @@
   <section class="grey">
     <div class="wrapper">
       <div class="row center-xs">
-        <div class="col-xs-12">
-          @include('partials.page-header')
-          @if (!have_posts())
-            <div class="alert alert-warning">
-              {{ __('Sorry, no results were found.', 'sage') }}
-            </div>
-            {!! get_search_form(false) !!}
-          @endif
+        @include('partials.page-header')
+      </div>
+      <div class="row">
+        @if (!have_posts())
+          <div class="alert alert-warning col-xs-12">
+            {{ __('Sorry, no results were found.', 'sage') }}
+          </div>
+          {!! get_search_form(false) !!}
+        @endif
 
-          @while (have_posts()) @php the_post() @endphp
-            <div class="col-md-6 col-xs-12">
-              <div class="box">
-                @include('partials.content')
-              </div>
+        @while (have_posts()) @php the_post() @endphp
+          <div class="col-md-6 col-xs-12">
+            <div class="box">
+              @include('partials.content')
             </div>
-          @endwhile
-          @if($wp_query->current_post % 2 == 1)
-            <div class="col-md-6 col-xs-12 end-card">
-              *
-            </div>
-          @endif
+          </div>
+        @endwhile
+        @if($wp_query->current_post % 2 == 1)
+          <div class="col-md-6 col-xs-12 end-card">
+            *
+          </div>
+        @endif
 
-          {!! get_the_posts_navigation() !!}
-        </div>
+
+        {!! get_the_posts_navigation() !!}
       </div>
     </div>
   </section>
