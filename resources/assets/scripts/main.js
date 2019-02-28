bbc1a64dabab8c9872ef0343f50710530abb0fb9
Carly Ho <cho@cliquestudios.com>
2019-02-27 18:22:15 -0600

    Add dependency, trigger on window.load

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -3,6 +3,7 @@ import 'jquery';
 import 'datatables.net-dt';
 import 'jquery-accessible-modal-window-aria';
 import 'trumbowyg';
+import 'clndr';
 
 // Import everything from autoload
 import "./autoload/**/*"
--- resources/assets/scripts/routes/calendar.js
+++ resources/assets/scripts/routes/calendar.js
@@ -1,16 +1,18 @@
 export default {
     init() {
-        $.ajax({
-            url: ajaxurl,
-            method: 'POST',
-            data: {
-                action: 'get_events',
-            },
-            success: function(data) {
-                $('#calendar').clndr({
-                    events: data,
+        $(window).load(function() {
+            $.ajax({
+              url: ajaxurl,
+              method: "POST",
+              data: {
+                action: "get_events"
+              },
+              success: function(data) {
+                $("#calendar").clndr({
+                  events: data
                 });
-            },
+              }
+            });
         });
     },
 }
