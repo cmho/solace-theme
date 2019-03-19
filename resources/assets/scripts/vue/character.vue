c48d0431b3e8781cb9fb0d698b35ae19d0247c9a
Carly Ho <yaysunshine@gmail.com>
2019-03-18 20:24:48 -0500

    remove components enclosure on character.vue

--- resources/assets/scripts/vue/character.vue
+++ resources/assets/scripts/vue/character.vue
@@ -8,9 +8,6 @@
     import Conditions from './conditions.vue';
 
     export default {
-        components: {
-            Character
-        },
         data () {
             return {
                 conditions: [],
