956cadbb8ccb1c8182e3f6bc4fc5cb01ad39d2ed
Carly Ho <yaysunshine@gmail.com>
2019-03-18 20:38:37 -0500

    add vueify

--- resources/assets/scripts/vue/character.vue
+++ resources/assets/scripts/vue/character.vue
@@ -3,3 +3,4 @@
 
     </li>
 </template>
+
--- resources/assets/scripts/vue/characters.vue
+++ resources/assets/scripts/vue/characters.vue
@@ -1,5 +1,24 @@
 <template>
-    <ol class="characters" v-if="info.characters">
-        <character v-for="character in info.characters" :key="character.ID"></character>
-    </ol>
+  <ol class="characters" v-if="info.characters">
+      <character v-for="character in info.characters" :key="character.ID"></character>
+  </ol>
 </template>
+
+<script>
+import axios from 'axios';
+import Character from './character.vue';
+
+export default {
+  data: {
+    characters: [],
+  },
+  components: {
+    Character
+  },
+  methods: function() {
+    axios.get('/wp-json/solace/v1/dashboard/characters').then(function(response) {
+      self.data.characters = JSON.parse(response);
+    });
+  }
+}
+</script>
