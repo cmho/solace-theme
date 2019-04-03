59d9c8ad3b656ca70097a3776245de9d006c65b7
Carly Ho <cho@cliquestudios.com>
2019-04-03 16:29:01 -0500

    add more debug data

--- app/controllers/characters.php
+++ app/controllers/characters.php
@@ -110,6 +110,12 @@ class Characters extends Controller
                     return false;
                 });
                 print_r($mer);
+                print_r(array(
+                        'character' => $char->ID,
+                        'link' => get_permalink($char->ID),
+                        'name' => $char->post_title,
+                        'rating' => $m['rating']
+                ));
                 if (count($mer) > 0) {
                     array_push($chars_by_initiation[$initiation->post_title], array(
                         'character' => $char->ID,
