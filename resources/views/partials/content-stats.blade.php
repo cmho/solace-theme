836dac870135e61f70bceb78f22a650264abe6a1
Carly Ho <cho@cliquestudios.com>
2019-04-03 16:38:59 -0500

    fix formatting

--- resources/views/partials/content-stats.blade.php
+++ resources/views/partials/content-stats.blade.php
@@ -1,15 +1,24 @@
-<h2>Characters by Family</h2>
-<div class="row">
-    @foreach(App\Characters::getInitiations() as $fam => $members)
-        <div class="col-md-4 col-xs-12">
-            <h3>{{ $fam }}</h3>
-            <ul>
-                @foreach($members as $member)
-                    <li>{{ $member['rating'] > 3 ? '<strong>' : '' }}<a href="{{ $member['link'] }}">{{ $member['name'] }}</a>{{ $member['rating'] > 3 ? '</strong>' : '' }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endforeach
-</div>
+<section>
+    <div class="wrapper">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2>Characters by Family</h2>
+                <div class="row">
+                    @foreach(App\Characters::getInitiations() as $fam => $members)
+                        <div class="col-md-4 col-xs-12">
+                            <h3>{{ $fam }}</h3>
+                            <ul>
+                                @foreach($members as $member)
+                                    <li>{{ $member['rating'] > 3 ? '<strong>' : '' }}<a href="{{ $member['link'] }}">{{ $member['name'] }}</a>{{ $member['rating'] > 3 ? '</strong>' : '' }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endforeach
+                </div>
 
-<h2>Skill Spread</h2>
+
+                <h2>Skill Spread</h2>
+            </div>
+        </div>
+    </div>
+</section>
