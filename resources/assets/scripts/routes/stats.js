e9274b730f8e54cb5db1f6f7e55417a4566660da
Carly Ho <cho@cliquestudios.com>
2019-04-04 17:03:46 -0500

    reimport chart.js

--- resources/assets/scripts/routes/stats.js
+++ resources/assets/scripts/routes/stats.js
@@ -1,56 +1,59 @@
+import Chart from 'chartjs';
+
 export default {
     init () {
-        $(window).on('load', function() {
-            $("#skills-row canvas").each(function(i, elt) {
-              var data = $(this)
-                .data("points")
-                .split(",")
-                .map(function(x) {
-                  return parseInt(x);
-                });
-              var chart = new Chart($(this), {
-                type: "bar",
-                data: {
-                  labels: [0, 1, 2, 3, 4, 5],
-                  datasets: [
+        $(window).on("load", function() {
+          $("#skills-row canvas").each(function() {
+            var id = $(this).attr("id");
+            var data = $(this)
+              .data("points")
+              .split(",")
+              .map(function(x) {
+                return parseInt(x);
+              });
+            new Chart($(this), {
+              type: "bar",
+              data: {
+                labels: [0, 1, 2, 3, 4, 5],
+                datasets: [
+                  {
+                    data: data,
+                    fill: false,
+                    backgroundColor: "yellow",
+                    borderWidth: 1
+                  }
+                ]
+              },
+              options: {
+                scales: {
+                  yAxes: [
                     {
-                      data: data,
-                      fill: false,
-                      backgroundColor: "yellow",
-                      borderWidth: 1
+                      gridLines: {
+                        drawTicks: false,
+                        color: "rgba(255,255,255,.5)"
+                      },
+                      ticks: {
+                        beginAtZero: true
+                      },
+                      labels: {
+                        fontColor: "white"
+                      }
+                    }
+                  ],
+                  xAxes: [
+                    {
+                      gridLines: {
+                        color: "rgba(255,255,255,.5)"
+                      }
                     }
                   ]
                 },
-                options: {
-                  scales: {
-                    yAxes: [
-                      {
-                        gridLines: {
-                          drawTicks: false,
-                          color: "rgba(255,255,255,.5)"
-                        },
-                        ticks: {
-                          beginAtZero: true
-                        },
-                        labels: {
-                          fontColor: "white"
-                        }
-                      }
-                    ],
-                    xAxes: [
-                      {
-                        gridLines: {
-                          color: "rgba(255,255,255,.5)"
-                        }
-                      }
-                    ]
-                  },
-                  legend: {
-                    display: false
-                  }
+                legend: {
+                  display: false
                 }
-              });
+              }
             });
+          });
         });
     }
 }
