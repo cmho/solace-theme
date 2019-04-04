07985a3287278c45fcec407606eee7912d4520a4
Carly Ho <cho@cliquestudios.com>
2019-04-03 20:07:53 -0500

    more debugging

--- app/controllers/characters.php
+++ app/controllers/characters.php
@@ -163,7 +163,9 @@ class Characters extends Controller
         );
         $spreads = array();
         $characters = \App\Characters::getActivePCs();
-        
+        foreach ($skills as $sk) {
+            $spreads[$sk] = array(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0);
+        }
         return $spreads;
     }
 }
