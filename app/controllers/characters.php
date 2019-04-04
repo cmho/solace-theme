3a9c4ead5c3775e51b11f8857dcfffff2343974e
Carly Ho <cho@cliquestudios.com>
2019-04-03 19:52:14 -0500

    fix class ref

--- app/controllers/characters.php
+++ app/controllers/characters.php
@@ -162,7 +162,7 @@ class Characters extends Controller
             'subterfuge'
         );
         $spreads = array();
-        $characters = Characters::getActivePCs();
+        $characters = \App\Characters::getActivePCs();
         foreach ($skills as $sk) {
             $spreads[$sk] = array(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0);
             foreach ($characters as $char) {
