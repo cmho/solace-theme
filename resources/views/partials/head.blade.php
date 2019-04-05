e2d2389880141586a70b27b9dc156266bdf6defc
Carly Ho <yaysunshine@gmail.com>
2019-04-04 21:04:05 -0500

    try and fix the article: author issue

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="theme-color" content="#333333" />
+  <meta name="article:author" content="" />
   <script type="text/javascript">
     var ajaxurl = "{{ admin_url('admin-ajax.php') }}";
     var themepath = "{{ get_theme_file_uri() }}";
