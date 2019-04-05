9bc10fa5afaabe03e00b3b66da46bc8e7f5de9df
Carly Ho <yaysunshine@gmail.com>
2019-04-04 20:10:59 -0500

    remove [0] label

--- app/controllers/characters.php
+++ app/controllers/characters.php
@@ -186,8 +186,8 @@ class Characters extends Controller
         $spreads = array();
         foreach($merits as $merit) {
             $spreads[$merit->post_title] = array(
-                'counts' => array(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0),
-                'characters' => array(0 => array(), 1 => array(), 2 => array(), 3 => array(), 4 => array(), 5 => array())
+                'counts' => array(1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0),
+                'characters' => array(1 => array(), 2 => array(), 3 => array(), 4 => array(), 5 => array())
             );
 
             foreach ($characters as $char) {
--- resources/assets/scripts/routes/stats.js
+++ resources/assets/scripts/routes/stats.js
@@ -3,7 +3,7 @@ import Chart from 'chart.js';
 export default {
     init () {
         $(window).on("load", function() {
-          $("#skills-row canvas, #merits-row canvas").each(function() {
+          $("#skills-row canvas").each(function() {
             var id = $(this).attr("id");
             var data = $(this)
               .data("points")
@@ -47,6 +47,51 @@ export default {
                       }
                     }
                   ]
+                },
+                legend: {
+                  display: false
+                }
+              }
+            });
+          });
+          $("#merits-row canvas").each(function () {
+            var id = $(this).attr("id");
+            var data = $(this)
+              .data("points")
+              .split(",")
+              .map(function (x) {
+                return parseInt(x);
+              });
+            new Chart($(this), {
+              type: "bar",
+              data: {
+                labels: [1, 2, 3, 4, 5],
+                datasets: [{
+                  data: data,
+                  fill: false,
+                  backgroundColor: "yellow",
+                  borderWidth: 1
+                }]
+              },
+              options: {
+                scales: {
+                  yAxes: [{
+                    gridLines: {
+                      drawTicks: false,
+                      color: "rgba(255,255,255,.5)"
+                    },
+                    ticks: {
+                      beginAtZero: true
+                    },
+                    labels: {
+                      fontColor: "white"
+                    }
+                  }],
+                  xAxes: [{
+                    gridLines: {
+                      color: "rgba(255,255,255,.5)"
+                    }
+                  }]
                 },
                 legend: {
                   display: false
