9f526720ee1b92a09228005ec2b5dbd8d35cc837
Carly Ho <yaysunshine@gmail.com>
2019-04-04 19:45:16 -0500

    debug tooltips

--- resources/assets/scripts/routes/stats.js
+++ resources/assets/scripts/routes/stats.js
@@ -12,6 +12,7 @@ export default {
                 return parseInt(x);
               });
             var tooltips = $(this).data('characters').split(";");
+            console.log(tooltips);
             new Chart($(this), {
               type: "bar",
               data: {
