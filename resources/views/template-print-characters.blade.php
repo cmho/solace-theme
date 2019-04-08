470dff4964123d967eefba1ea17244e66e618d68
Carly Ho <cho@cliquestudios.com>
2019-04-08 12:16:25 -0500

    fix stylesheets for print

--- resources/views/template-print-characters.blade.php
+++ resources/views/template-print-characters.blade.php
@@ -12,7 +12,7 @@
   @foreach(App\Characters::activeList() as $post)
     @php(setup_postdata($post))
     <article>
-      @include('partials.content-single-character')
+      @include('partials.content-single-character', ['print' => true])
     </article>
   @endforeach
   @php(wp_reset_postdata())
