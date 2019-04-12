1c3c6cabbc5c8c59cfb16da7c53efa1cc0f43044
Carly Ho <cho@cliquestudios.com>
2019-04-12 11:45:51 -0500

    format equipment list correctly

--- resources/views/partials/content-single-character.blade.php
+++ resources/views/partials/content-single-character.blade.php
@@ -161,21 +161,7 @@
           @if(get_field('equipment'))
             <ul class="equipment">
               @foreach(get_field('equipment') as $equipment)
-              <li>
-                <strong>{{ get_the_title($equipment['item']->ID) }}</strong><br />
-                <dl>
-                  <dt>Type</dt>
-                  <dd>{{ get_field('type', $equipment['item']->ID) }}</dd>
-                  <dt>Size</dt>
-                  <dd>{{ get_field('size', $equipment['item']->ID) }}</dd>
-                  <dt>Durability</dt>
-                  <dd>{{ get_field('durability', $equipment['item']->ID) }}</dd>
-                  <dt>Qualities</dt>
-                  <dd>{{ join(", ", get_field('qualities', $equipment['item']->ID)) }}</dd>
-                  <dt>Notes</dt>
-                  <dd>{{ $equipment['note'] }}</dd>
-                </dl>
-              </li>
+                @include('partials.content-equipment-list')
               @endforeach
             </ul>
           @else
